0e1242e6cec32d26d57da6aadf2c77a7095eed25
MM58-crypto <mohammed.magdi999@gmail.com>
2026-04-30 23:14:31 +0300

    .....

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+cout << " Megaman CLI -- display Megaman sprites in your terminal  \n"
+
+void ShowRandMM() {
+     // randomly display a megaman character
+     // access the characters from a data structure / file / folder
+
+
+
+}
+
+void DisplayList() {
+     // print a list of all the characters available 
+
+
+}
+
+return 0;
+}
