73da58a745ddacfda37296a15c8d69492abac734
MM58-crypto <mohammed.magdi999@gmail.com>
2026-04-30 23:16:04 +0300

    wooo another commit

---
+++ auto_git.sh
@@ -0,0 +1,9 @@
+
+# - quick simple script for pushing changes to github instead of me re-writing the same exact commands every single time, lel :O
+
+
+git add .
+git commit -m  "wooo another commit"
+git push -u origin main 
+
+
