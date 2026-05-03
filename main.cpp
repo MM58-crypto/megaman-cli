e3271716fc843c61fbd21a4a3da0d4f581b7e6af
MM58-crypto <mohammed.magdi999@gmail.com>
2026-05-03 22:21:27 +0300

    wooo another commit

--- main.cpp
+++ main.cpp
@@ -1,24 +1,58 @@
 #include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<string>
+
 using namespace std;
-
-
-int main() {
-
-cout << " Megaman CLI -- display Megaman sprites in your terminal  \n"
 
 void ShowRandMM() {
      // randomly display a megaman character
      // access the characters from a data structure / file / folder
-
+     string mm_sprites_path;
 
 
 }
 
 void DisplayList() {
-     // print a list of all the characters available 
+     // print a list of all the characters available
 
 
 }
+
+void Help() {
+
+    int option;
+    cout<<"***********************************************************************"<<endl;
+    cout<<"*                               HELP MENU                                 *"<<endl;
+    cout<<"***********************************************************************"<<endl;
+    cout<< "   "<<endl;
+    cout<< "Please choose an option"<<endl;
+    cout<< "1.  "<<endl;
+    cout<< "2.  " <<endl;
+    cout<< "3. Exit" << endl;
+    cin >> option;
+
+
+    switch(option) {
+         case 1:
+            // some logic
+
+         case 2:
+            // some logic
+            //
+
+         case 3:
+              break;
+
+    }
+}
+
+
+int main() {
+
+cout << " Megaman CLI -- display Megaman sprites on your terminal  " <<endl;
+
+
 
 return 0;
 }
