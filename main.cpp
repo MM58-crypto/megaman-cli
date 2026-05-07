396c80bbeef648b79a760d78ca437d1e55f0bb74
MM58-crypto <mohammed.magdi999@gmail.com>
2026-05-07 13:01:06 +0300

    wooo another commit

--- main.cpp
+++ main.cpp
@@ -8,27 +8,30 @@ using namespace std;
 void ShowRandMM() {
      // randomly display a megaman character
      // access the characters from a data structure / file / folder
-     string mm_sprites_path;
-
+     string mm_sprites_path = "/mm-sprites/"; // --
+     ofstream sprites;
+     // current idea - for loop through sprites dir, select a random sprite and display it
 
 }
 
 void DisplayList() {
      // print a list of all the characters available
-
+     // get the file name of each sprite, remove file extenstion
 
 }
+
+
 
 void Help() {
 
     int option;
-    cout<<"***********************************************************************"<<endl;
+    cout<<"***************************************************************************"<<endl;
     cout<<"*                               HELP MENU                                 *"<<endl;
-    cout<<"***********************************************************************"<<endl;
+    cout<<"***************************************************************************"<<endl;
     cout<< "   "<<endl;
     cout<< "Please choose an option"<<endl;
-    cout<< "1.  "<<endl;
-    cout<< "2.  " <<endl;
+    cout<< "1. Randomly display a Mega Man X character  "<<endl;
+    cout<< "2. List of all available Mega Man X Characters " <<endl;
     cout<< "3. Exit" << endl;
     cin >> option;
 
